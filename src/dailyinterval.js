df73847ea3d3034957cf9fe9294b30b0c30795e7
TelevisionNinja <TelevisionNinja@users.noreply.github.com>
2022-03-18 16:44:32 -0500

    rewrite of the program

--- src/dailyinterval.js
+++ src/dailyinterval.js
@@ -1,267 +1,233 @@
-import {
-    setNoDriftTimeout,
-    clearNoDrift
-} from 'no-drift';
+// collection of IDs so that the timers can be cleared
+const IDs = new Map();
+// variable to keep track of and return a new ID
+let newID = 1;
+const rate = 0.9;
+// number of ms in a min
+const conversionFactor = 60 * 1000;
 
-export default class DailyInterval {
-    // private vars
-    #currentTime;
-    #currentMs;
-    #currentInterval;
-    #timeoutID;
-
-    // user's vars
-    #func;
-    #intervalStartTime;
-    #interval;
-    #offset;
-
-    // timimg vars
-    #timeRemaining;
-    //#isNow;
-    //#overshoot;
-    //#undershoot;
-
-    //----------------------------------------------------------------------------------
-    // constructor
-
-    /**
-     * constructs the interval object
-     * 
-     * @param {Function} func func to execute at every interval
-     * @param {String} intervalStartTime starting time of the intervals, format: h:m (24 hour)
-     * @param {Number} interval minutes between intervals
-     * @param {Number} offset ms offset
-     */
-    constructor(func, intervalStartTime, interval, offset = 0) {
-        // user's vars
-        this.func = func;
-        this.intervalStartTime = intervalStartTime;
-        this.interval = interval;
-        this.offset = offset;
-
-        // private vars
-        this.#currentTime = 0;
-        this.#currentMs = 0;
-        this.#currentInterval = 0;
-        this.#timeoutID = undefined;
-
-        // timimg vars
-        this.#timeRemaining = 0;
-        //this.#isNow = false;
-        //this.#overshoot = false;
-        //this.#undershoot = false;
+/**
+ * creates a function if the callback is a string
+ * 
+ * @param {Function} callback 
+ * @param {...any} args 
+ * @returns 
+ */
+function createCallback(callback, args) {
+    if (typeof callback === 'function') {
+        return () => callback(...args);
     }
 
-    //----------------------------------------------------------------------------------
-    // getters
+    return Function(...args, callback);
+}
 
-    /**
-     * returns the interval value
-     */
-    get interval() {
-        return this.#interval;
+/**
+ * 
+ * @param {String} str format: 24 hr time h:m
+ * @returns array
+ */
+function parseTimeStr(str) {
+    return str.split(':').map(i => parseInt(i));
+}
+
+/**
+ * converts hrs and mins to mins
+ * @param {Number} hrs 
+ * @param {Number} mins 
+ * @returns mins
+ */
+function convertTimeToMs(hrs, mins) {
+    return (60 * hrs + mins) * conversionFactor;
+}
+
+/**
+ * 
+ * @param {Array} arr array from parseTimeStr()
+ * @returns ms
+ */
+function timeArrToMs(arr) {
+    return convertTimeToMs(...arr);
+}
+
+/**
+ * converts ms to hrs and mins
+ * 
+ * @param {Number} ms 
+ * @returns hrs and mins object
+ */
+function convertMsToHrAndMins(ms) {
+    const mins = Math.trunc(ms / conversionFactor);
+
+    return {
+        hrs: Math.trunc(mins / 60),
+        mins: mins % 60
+    };
+}
+
+/**
+ * converts mins to ms
+ * @param {Number} mins mins
+ * @returns ms
+ */
+function convertMinsToMs(mins) {
+    return conversionFactor * mins;
+}
+
+/**
+ * returns the time (hours and minutes) in ms
+ * 
+ * @param {Object} time Date object
+ * @returns ms
+ */
+function getTimeInMs(time) {
+    return convertTimeToMs(time.getHours(), time.getMinutes()); 
+}
+
+/**
+ * sets the correct time for the intervalTime object
+ * 
+ * @param {Object} intervalTime Data object
+ * @param {Number} interval ms
+ */
+function adjustIntervalTime(intervalTime, interval) {
+    const correctIntervalTime = convertMsToHrAndMins(Math.trunc(getTimeInMs(intervalTime) / interval) * interval);
+
+    const currentHr = intervalTime.getHours();
+    const currentMin = intervalTime.getMinutes();
+    const hrDelta = correctIntervalTime.hrs - currentHr;
+    const minDelta = correctIntervalTime.mins - currentMin;
+    const previous = intervalTime.valueOf();
+
+    intervalTime.setHours(currentHr + hrDelta, currentMin + minDelta);
+
+    if (intervalTime.valueOf() < Date.now()) {
+        intervalTime.setTime(previous + interval);
+        adjustIntervalTime(intervalTime, interval);
     }
+}
 
-    /**
-     * returns the interval starting time value
-     */
-    get intervalStartTime() {
-        return this.#intervalStartTime;
-    }
+/**
+ * calculates and sets the next interval time based on a given current time, interval amount, and the interval starting time
+ * 
+ * @param {Number} intervalTime Date object
+ * @param {Number} interval ms
+ * @param {Number} epoch ms
+ */
+function calcNextIntervalTime(intervalTime, interval, epoch) {
+    /*
+        // formula
+        // 'interval' and 'startTime' are in some selected unit
+        // division is integer division
 
-    /**
-     * returns the offset
-     */
-    get offset() {
-        return this.#offset;
-    }
+        // remove offset (startTime)
+        delta = currentTime - startTime
 
-    //----------------------------------------------------------------------------------
-    // setters
+        n = delta / interval
 
-    /**
-     * sets the mins between intervals
-     * the lowest value is 1
-     * 
-     * @param {Number} newInterval mins between intervals
-     */
-    set interval(newInterval) {
-        // prevent less than 1 minute intervals
-        if (newInterval < 1) {
-            newInterval = 1;
+        // for negative deltas, return the nearest interval
+        if (delta >= 0) {
+            n++
         }
 
-        this.#interval = newInterval;
+        nextTime = interval * n + startTime
+
+        // the result is the next time interval in the appropriate untis
+    */
+
+    const delta = Date.now() - epoch;
+    let nearestInterval = Math.trunc(delta / interval);
+
+    if (delta >= 0) {
+        nearestInterval++;
     }
 
-    /**
-     * sets the function to be executed
-     * 
-     * @param {Function} newFunc function to be executed
-     */
-    set func(newFunc) {
-        this.#func = newFunc;
-    }
+    intervalTime.setTime(nearestInterval * interval + epoch);
+    // the set time coud have the wrong hrs and mins
+    adjustIntervalTime(intervalTime, interval);
+}
 
-    /**
-     * sets the starting time of the intervals
-     * 
-     * @param {String} newIntervalStartTime starting time of the intervals, format: h:m (24 hour)
-     */
-    set intervalStartTime(newIntervalStartTime) {
-        const startTimeArr = newIntervalStartTime.split(':').map(i => parseInt(i));
-        this.#intervalStartTime = 60 * startTimeArr[0] + startTimeArr[1];
-    }
+/**
+ * creates a time interval Date object
+ * 
+ * @param {Number} interval ms
+ * @param {Number} epoch ms
+ * @returns Date object
+ */
+function createTimeInterval(interval, epoch) {
+    const intervalTime = new Date();
+    calcNextIntervalTime(intervalTime, interval, epoch);
+    return intervalTime;
+}
 
-    /**
-     * sets the offset
-     * 
-     * @param {Number} newOffset
-     */
-    set offset(newOffset) {
-        this.#offset = newOffset;
-    }
+/**
+ * calls timeout repeatedly
+ * 
+ * @param {Function} callback function
+ * @param {Number} ID int
+ * @param {Number} interval ms
+ * @param {Object} intervalTime Data object
+ * @param {Number} epoch ms
+ */
+function customInterval(callback, ID, interval, intervalTime, epoch) {
+    IDs.set(
+        ID,
+        setTimeout(() => {
+            const time = Date.now();
 
-    //----------------------------------------------------------------------------------
-    // private methods
+            if (intervalTime.valueOf() <= time) {
+                intervalTime.setTime(intervalTime.valueOf() + interval);
 
-    /**
-     * calculates the ms left until the current interval
-     * if the current time is the current interval, the 'isNow' boolean will be true, and the time remaining will be the provided interval
-     * if the current time is after the current interval, the boolean 'overshoot' will be true, and the time remaining will be the time until the next interval
-     * if the current time is before the current interval, the boolean 'undershoot' will be true, and the time remaining will be the time until the current interval
-     * 
-     * the time is 24 hr time
-     */
-    #getMsUntilIntervalTime() {
-        // reset booleans
-        //this.#isNow = false;
-        //this.#overshoot = false;
-        //this.#undershoot = false;
-
-        let delta = this.#currentInterval - this.#currentTime;
-
-        if (!delta) {
-            // now
-            delta = this.#interval;
-            //this.#isNow = true;
-        }
-        else if (delta < 0) {
-            // overshoot
-            delta += (~~(-delta / this.#interval) + 1) * this.#interval;
-            //this.#overshoot = true;
-        }
-
-        this.#timeRemaining = 60000 * delta - this.#currentMs + this.#offset;
-    }
-
-    /**
-     * calculates the next interval time based on the current time and the interval starting time
-     */
-    #calcNextIntervalTime() {
-        /*
-            // formula
-            // 'interval' and 'startTime' are assumed to be in minutes
-            // division is integer division
-
-            // remove offset (startTime)
-            delta = currentTime - startTime
-
-            // assumed every day is 24 hours
-            // add 24 hours in mins (carry) to negative numbers
-            if (delta < 0) {
-                delta += 1440
+                if (intervalTime.valueOf() < time) {
+                    // system time changes greater than the interval time
+                    calcNextIntervalTime(intervalTime, interval, epoch);
+                }
+                else {
+                    callback();
+                    // daylight savings adjustment
+                    adjustIntervalTime(intervalTime, interval);
+                }
+            }
+            else {
+                // system time changes less than the interval time
+                if (intervalTime.valueOf() - time > interval) {
+                    calcNextIntervalTime(intervalTime, interval, epoch);
+                }
             }
 
-            n = delta / interval + 1
+            customInterval(callback, ID, interval, intervalTime, epoch);
+        }, rate * (intervalTime.valueOf() - Date.now()))
+    );
+}
 
-            nextTime = interval * n + startTime
-
-            // convert to hours and minutes
-
-            newHr = nextTime / 60 % 24
-
-            newMin = nextTime % 60
-        */
-
-        let delta = this.#currentTime - this.#intervalStartTime;
-
-        if (delta < 0) {
-            delta += 1440;
-        }
-
-        this.#currentInterval = ((~~(delta / this.#interval) + 1) * this.#interval + this.#intervalStartTime) % 1440;
+/**
+ * calls timeout repeatedly
+ * 
+ * @param {Function} callback func to execute at every interval
+ * @param {Number} interval mins between intervals, default: 1
+ * @param {String} startingTime starting time of the intervals, format: 24 hour time 'h:m', default value: '0:0'
+ * @param  {...any} args args of the callback func
+ * @returns an ID
+ */
+export function setDailyInterval(callback, interval = 1, startingTime = '0:0', ...args) {
+    if (interval < 1) {
+        interval = 1;
     }
 
-    /**
-     * updates the current time in minutes and current second and millisecond in milliseconds
-     */
-    #updateTime() {
-        const time = new Date();
-        this.#currentTime = 60 * time.getHours() + time.getMinutes();
-        this.#currentMs = 1000 * time.getSeconds() + time.getMilliseconds();
-    }
+    interval = convertMinsToMs(interval);
+    const epoch = timeArrToMs(parseTimeStr(startingTime));
 
-    /**
-     * creates the interval
-     */
-    #createInterval() {
-        // update the time
-        this.#updateTime();
+    // start the interval
+    customInterval(createCallback(callback, args), newID, interval, createTimeInterval(interval, epoch), epoch);
 
-        // calculate the next interval
-        this.#calcNextIntervalTime();
+    return newID++;
+}
 
-        // calculate the time remaining until the next interval
-        this.#getMsUntilIntervalTime();
-
-        // wait until the interval time
-        this.#timeoutID = setNoDriftTimeout(() => {
-            // execute the function
-            this.#func();
-
-            this.#createInterval();
-        }, this.#timeRemaining);
-    }
-
-    //----------------------------------------------------------------------------------
-    // public methods
-
-    /**
-     * starts the interval
-     * if the given start time is the current time, the function will be executed on the next interval
-     * the time is 24 hr time
-     * precision is of a minute
-     * 
-     * @param {Boolean} executeNow execute the function on the 'start()' call, default value is false
-     */
-    start(executeNow = false) {
-        if (executeNow) {
-            this.#func();
-        }
-
-        // create the interval
-        this.#createInterval();
-    }
-
-    /**
-     * stops the current running interval
-     */
-    stop() {
-        // cancel the timeout
-        clearNoDrift(this.#timeoutID);
-
-        // clear private vars
-        this.#currentTime = 0;
-        this.#currentMs = 0;
-        this.#currentInterval = 0;
-        this.#timeoutID = undefined;
-
-        // clear timimg vars
-        this.#timeRemaining = 0;
-        //this.#isNow = false;
-        //this.#overshoot = false;
-        //this.#undershoot = false;
-    }
+/**
+ * cancels a daily interval
+ * 
+ * @param {Number} ID 
+ */
+export function clearDailyInterval(ID) {
+    clearTimeout(IDs.get(ID));
+    IDs.delete(ID);
 }
